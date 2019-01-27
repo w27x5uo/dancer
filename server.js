c153640b50c1648cfc22cccc1054f5e2c82a587d
w27x5uo <47054369+w27x5uo@users.noreply.github.com>
2019-01-27 14:57:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const server = http.createServer(function (req, res) {
 		res.writeHead(200, {
 			'Content-Type': 'text/plain'
 		});
-		res.end('dancerist');
+		res.end(cryptor.randomBytes(Math.floor(Math.random() * 16) + 1).toString());
 	});
 const wss = new WebSocketServer({
 		server
